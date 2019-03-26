8bcff3d81ba5069a800866d9fdcfecabc0b3058c
mokmalla <shl1933@naver.com>
2019-03-26 19:46:48 +0900

    add Echo project

--- 20190326/src/Echo.java
+++ 20190326/src/Echo.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class Echo {
-
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String inputString;
+	String Echo;
+	while(true) {
+		System.out.println("아무말이나 하시오.");
+		Echo = sc.nextLine();
+		System.out.println(Echo);
+		if(Echo.equals("exit")) {
+			System.out.println("끝"); break;
+		}
+	}
+}
 }
