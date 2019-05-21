6a6f4eb1d02896078c14992c0e6d61584ca9a10b
mokmalla <shl1933@naver.com>
2019-05-21 18:56:57 +0900

    add JRadioButton, buttonGroup

---
+++ 20190507_GUI/src/dd.java
@@ -0,0 +1,18 @@
+
+public class dd {
+	public static void main(String[] args) {
+	
+				int score = 100;
+				char rank;
+				if (score >= 90 && score <=100 )
+				  rank='A';
+				else if(score >= 80 && score <90)
+				 rank = 'B';
+				else if(score >= 70 && score <80)
+				 rank = 'C';
+				else
+				 rank ='D';
+
+				System.out.println(rank);
+	}
+}
